llvm-hs/llvm-hs

Cleanup. Simplify approach to PassSetSpecs.

src/LLVM/General/Internal/PassManager.hs
@@ -13,14 +13,13 @@ import Control.Applicative
 
 import Control.Monad.AnyCont
 
+import Data.Word
 import Data.Foldable (forM_)
-import Data.Word (Word)
 import Foreign.C (CString)
 import Foreign.Ptr
 
 import qualified LLVM.General.Internal.FFI.PassManager as FFI
 import qualified LLVM.General.Internal.FFI.Transforms as FFI
-import qualified LLVM.General.Internal.FFI.LLVMCTypes as FFI
 
 import LLVM.General.Internal.Module
 import LLVM.General.Internal.Target
@@ -31,99 +30,98 @@ import LLVM.General.Transforms
 import LLVM.General.AST.DataLayout
 
 -- | <http://llvm.org/doxygen/classllvm_1_1PassManager.html>
+-- Note: a PassManager does substantive behind-the-scenes work, arranging for the
+-- results of various analyses to be available as needed by transform passes, shared
+-- as possible.
 newtype PassManager = PassManager (Ptr FFI.PassManager)
 
--- | There are different ways to get a 'PassManager'. This class embodies them.
-class PassManagerSpecification s where
-    -- | make a 'PassManager'
-    createPassManager :: s -> IO (Ptr FFI.PassManager)
+-- | There are different ways to get a 'PassManager'. This type embodies them.
+data PassSetSpec
+  -- | a 'PassSetSpec' is a lower-level, detailed specification of a set of passes. It
+  -- allows fine-grained control of what passes are to be run when, and the specification
+  -- of passes not available through 'CuratedPassSetSpec'.
+  = PassSetSpec {
+      transforms :: [Pass],
+      dataLayout :: Maybe DataLayout,
+      targetLibraryInfo :: Maybe TargetLibraryInfo,
+      targetMachine :: Maybe TargetMachine
+    }
+  -- | This type is a high-level specification of a set of passes. It uses the same
+  -- collection of passes chosen by the LLVM team in the command line tool 'opt'.  The fields
+  -- of this spec are much like typical compiler command-line flags - e.g. -O\<n\>, etc.
+  | CuratedPassSetSpec {
+      optLevel :: Maybe Word,
+      sizeLevel :: Maybe Word,
+      unitAtATime :: Maybe Bool,
+      simplifyLibCalls :: Maybe Bool,
+      useInlinerWithThreshold :: Maybe Word
+    }
 
--- | This type is a high-level specification of a set of passes. It uses the same
--- collection of passes chosen by the LLVM team in the command line tool 'opt'.  The fields
--- of this spec are much like typical compiler command-line flags - e.g. -O\<n\>, etc.
-data CuratedPassSetSpec = CuratedPassSetSpec {
-    optLevel :: Maybe Int,
-    sizeLevel :: Maybe Int,
-    unitAtATime :: Maybe Bool,
-    simplifyLibCalls :: Maybe Bool,
-    useInlinerWithThreshold :: Maybe Int
-  }
-
--- | Helper to make a 'CuratedPassSetSpec'
+-- | Helper to make a curated 'PassSetSpec'
 defaultCuratedPassSetSpec = CuratedPassSetSpec {
-    optLevel = Nothing,
-    sizeLevel = Nothing,
-    unitAtATime = Nothing,
-    simplifyLibCalls = Nothing,
-    useInlinerWithThreshold = Nothing
-  }
+  optLevel = Nothing,
+  sizeLevel = Nothing,
+  unitAtATime = Nothing,
+  simplifyLibCalls = Nothing,
+  useInlinerWithThreshold = Nothing
+}
 
-instance PassManagerSpecification CuratedPassSetSpec where
-  createPassManager s = bracket FFI.passManagerBuilderCreate FFI.passManagerBuilderDispose $ \b -> do
-    let handleOption g m = maybe (return ()) (g b . fromIntegral . fromEnum) (m s)
-    handleOption FFI.passManagerBuilderSetOptLevel optLevel
-    handleOption FFI.passManagerBuilderSetSizeLevel sizeLevel
-    handleOption FFI.passManagerBuilderSetDisableUnitAtATime (liftM not . unitAtATime)
-    handleOption FFI.passManagerBuilderSetDisableSimplifyLibCalls (liftM not . simplifyLibCalls)
-    handleOption FFI.passManagerBuilderUseInlinerWithThreshold useInlinerWithThreshold
-    pm <- FFI.createPassManager
-    FFI.passManagerBuilderPopulateModulePassManager b pm
-    return pm
-
-data PassSetSpec = PassSetSpec [Pass] (Maybe TargetMachine) (Maybe DataLayout) (Maybe TargetLibraryInfo)
-
-instance Monad m => EncodeM m (Maybe Bool) (FFI.NothingAsMinusOne Bool) where
-  encodeM = return . FFI.NothingAsMinusOne . maybe (-1) (fromIntegral . fromEnum)
-
-instance Monad m => EncodeM m (Maybe Word) (FFI.NothingAsMinusOne Word) where
-  encodeM = return . FFI.NothingAsMinusOne . maybe (-1) fromIntegral
+-- | an empty 'PassSetSpec'
+defaultPassSetSpec = PassSetSpec {
+  transforms = [],
+  dataLayout = Nothing,
+  targetLibraryInfo = Nothing,
+  targetMachine = Nothing
+}
 
 instance (Monad m, MonadAnyCont IO m) => EncodeM m GCOVVersion CString where
   encodeM (GCOVVersion cs@[_,_,_,_]) = encodeM cs
 
-instance PassManagerSpecification PassSetSpec where
-  createPassManager (PassSetSpec ps tm' dl tli) = flip runAnyContT return $ do
-    let tm = maybe nullPtr (\(TargetMachine tm) -> tm) tm'
-    pm <- liftIO $ FFI.createPassManager
-    forM_ tli $ \(TargetLibraryInfo tli) -> do
-      liftIO $ FFI.addTargetLibraryInfoPass pm tli
-    forM_ dl $ \dl -> liftIO $ withFFIDataLayout dl $ FFI.addDataLayoutPass pm 
-    forM ps $ \p -> $(
-      do
-        TH.TyConI (TH.DataD _ _ _ cons _) <- TH.reify ''Pass
-        TH.caseE [| p |] $ flip map cons $ \con -> do
-          let
-            (n, fns) = case con of
-                          TH.RecC n fs -> (n, [ TH.nameBase fn | (fn, _, _) <- fs ])
-                          TH.NormalC n [] -> (n, [])
-            actions = 
-              [ TH.bindS (TH.varP . TH.mkName $ fn) [| encodeM $(TH.dyn fn) |] | fn <- fns ]
-              ++ [
-               TH.noBindS [|
-                 liftIO $(
-                   foldl1 TH.appE
-                   (map TH.dyn $
-                      ["FFI.add" ++ TH.nameBase n ++ "Pass", "pm"]
-                      ++ ["tm" | FFI.needsTargetMachine (TH.nameBase n)]
-                      ++ fns)
-                  )
-                 |]
-               ]
-          TH.match (TH.conP n $ map (TH.varP . TH.mkName) fns) (TH.normalB (TH.doE actions)) []
-     )
-    return pm
-
-instance PassManagerSpecification [Pass] where
-  createPassManager ps = createPassManager (PassSetSpec ps Nothing Nothing Nothing)
-
-instance PassManagerSpecification ([Pass], TargetMachine) where
-  createPassManager (ps, tm) = createPassManager (PassSetSpec ps (Just tm) Nothing Nothing)
-
-instance PassManagerSpecification ([Pass], DataLayout, TargetLibraryInfo) where
-  createPassManager (ps, dl, tli) = createPassManager (PassSetSpec ps Nothing (Just dl) (Just tli))
+createPassManager :: PassSetSpec -> IO (Ptr FFI.PassManager)
+createPassManager pss = flip runAnyContT return $ do
+  pm <- liftIO $ FFI.createPassManager
+  case pss of
+    s@CuratedPassSetSpec {} -> liftIO $ do
+      bracket FFI.passManagerBuilderCreate FFI.passManagerBuilderDispose $ \b -> do
+        let handleOption g m = forM_ (m s) (g b <=< encodeM) 
+        handleOption FFI.passManagerBuilderSetOptLevel optLevel
+        handleOption FFI.passManagerBuilderSetSizeLevel sizeLevel
+        handleOption FFI.passManagerBuilderSetDisableUnitAtATime (liftM not . unitAtATime)
+        handleOption FFI.passManagerBuilderSetDisableSimplifyLibCalls (liftM not . simplifyLibCalls)
+        handleOption FFI.passManagerBuilderUseInlinerWithThreshold useInlinerWithThreshold
+        FFI.passManagerBuilderPopulateModulePassManager b pm
+    PassSetSpec ps dl tli tm' -> do
+      let tm = maybe nullPtr (\(TargetMachine tm) -> tm) tm'
+      forM_ tli $ \(TargetLibraryInfo tli) -> do
+        liftIO $ FFI.addTargetLibraryInfoPass pm tli
+      forM_ dl $ \dl -> liftIO $ withFFIDataLayout dl $ FFI.addDataLayoutPass pm 
+      forM_ ps $ \p -> $(
+        do
+          TH.TyConI (TH.DataD _ _ _ cons _) <- TH.reify ''Pass
+          TH.caseE [| p |] $ flip map cons $ \con -> do
+            let
+              (n, fns) = case con of
+                            TH.RecC n fs -> (n, [ TH.nameBase fn | (fn, _, _) <- fs ])
+                            TH.NormalC n [] -> (n, [])
+              actions = 
+                [ TH.bindS (TH.varP . TH.mkName $ fn) [| encodeM $(TH.dyn fn) |] | fn <- fns ]
+                ++ [
+                 TH.noBindS [|
+                   liftIO $(
+                     foldl1 TH.appE
+                     (map TH.dyn $
+                        ["FFI.add" ++ TH.nameBase n ++ "Pass", "pm"]
+                        ++ ["tm" | FFI.needsTargetMachine (TH.nameBase n)]
+                        ++ fns)
+                    )
+                   |]
+                 ]
+            TH.match (TH.conP n $ map (TH.varP . TH.mkName) fns) (TH.normalB (TH.doE actions)) []
+       )
+  return pm
 
 -- | bracket the creation of a 'PassManager'
-withPassManager :: PassManagerSpecification s => s -> (PassManager -> IO a) -> IO a
+withPassManager :: PassSetSpec -> (PassManager -> IO a) -> IO a
 withPassManager s = bracket (createPassManager s) FFI.disposePassManager . (. PassManager)
 
 -- | run the passes in a 'PassManager' on a 'Module', modifying the 'Module'.

bscarlet/llvm-general

Conflicts:
	src/LLVM/General/Internal/PassManager.hs
	src/LLVM/General/PassManager.hs
	test/LLVM/General/Test/Optimization.hs

src/LLVM/General/Internal/PassManager.hs
@@ -13,14 +13,13 @@ import Control.Applicative
 
 import Control.Monad.AnyCont
 
+import Data.Word
 import Data.Foldable (forM_)
-import Data.Word (Word)
 import Foreign.C (CString)
 import Foreign.Ptr
 
 import qualified LLVM.General.Internal.FFI.PassManager as FFI
 import qualified LLVM.General.Internal.FFI.Transforms as FFI
-import qualified LLVM.General.Internal.FFI.LLVMCTypes as FFI
 
 import LLVM.General.Internal.Module
 import LLVM.General.Internal.Target
@@ -31,99 +30,98 @@ import LLVM.General.Transforms
 import LLVM.General.AST.DataLayout
 
 -- | <http://llvm.org/doxygen/classllvm_1_1PassManager.html>
+-- Note: a PassManager does substantive behind-the-scenes work, arranging for the
+-- results of various analyses to be available as needed by transform passes, shared
+-- as possible.
 newtype PassManager = PassManager (Ptr FFI.PassManager)
 
--- | There are different ways to get a 'PassManager'. This class embodies them.
-class PassManagerSpecification s where
-    -- | make a 'PassManager'
-    createPassManager :: s -> IO (Ptr FFI.PassManager)
+-- | There are different ways to get a 'PassManager'. This type embodies them.
+data PassSetSpec
+  -- | a 'PassSetSpec' is a lower-level, detailed specification of a set of passes. It
+  -- allows fine-grained control of what passes are to be run when, and the specification
+  -- of passes not available through 'CuratedPassSetSpec'.
+  = PassSetSpec {
+      transforms :: [Pass],
+      dataLayout :: Maybe DataLayout,
+      targetLibraryInfo :: Maybe TargetLibraryInfo,
+      targetLowering :: Maybe TargetLowering
+    }
+  -- | This type is a high-level specification of a set of passes. It uses the same
+  -- collection of passes chosen by the LLVM team in the command line tool 'opt'.  The fields
+  -- of this spec are much like typical compiler command-line flags - e.g. -O\<n\>, etc.
+  | CuratedPassSetSpec {
+      optLevel :: Maybe Word,
+      sizeLevel :: Maybe Word,
+      unitAtATime :: Maybe Bool,
+      simplifyLibCalls :: Maybe Bool,
+      useInlinerWithThreshold :: Maybe Word
+    }
 
--- | This type is a high-level specification of a set of passes. It uses the same
--- collection of passes chosen by the LLVM team in the command line tool 'opt'.  The fields
--- of this spec are much like typical compiler command-line flags - e.g. -O\<n\>, etc.
-data CuratedPassSetSpec = CuratedPassSetSpec {
-    optLevel :: Maybe Int,
-    sizeLevel :: Maybe Int,
-    unitAtATime :: Maybe Bool,
-    simplifyLibCalls :: Maybe Bool,
-    useInlinerWithThreshold :: Maybe Int
-  }
-
--- | Helper to make a 'CuratedPassSetSpec'
+-- | Helper to make a curated 'PassSetSpec'
 defaultCuratedPassSetSpec = CuratedPassSetSpec {
-    optLevel = Nothing,
-    sizeLevel = Nothing,
-    unitAtATime = Nothing,
-    simplifyLibCalls = Nothing,
-    useInlinerWithThreshold = Nothing
-  }
+  optLevel = Nothing,
+  sizeLevel = Nothing,
+  unitAtATime = Nothing,
+  simplifyLibCalls = Nothing,
+  useInlinerWithThreshold = Nothing
+}
 
-instance PassManagerSpecification CuratedPassSetSpec where
-  createPassManager s = bracket FFI.passManagerBuilderCreate FFI.passManagerBuilderDispose $ \b -> do
-    let handleOption g m = maybe (return ()) (g b . fromIntegral . fromEnum) (m s)
-    handleOption FFI.passManagerBuilderSetOptLevel optLevel
-    handleOption FFI.passManagerBuilderSetSizeLevel sizeLevel
-    handleOption FFI.passManagerBuilderSetDisableUnitAtATime (liftM not . unitAtATime)
-    handleOption FFI.passManagerBuilderSetDisableSimplifyLibCalls (liftM not . simplifyLibCalls)
-    handleOption FFI.passManagerBuilderUseInlinerWithThreshold useInlinerWithThreshold
-    pm <- FFI.createPassManager
-    FFI.passManagerBuilderPopulateModulePassManager b pm
-    return pm
-
-data PassSetSpec = PassSetSpec [Pass] (Maybe TargetLowering) (Maybe DataLayout) (Maybe TargetLibraryInfo)
-
-instance Monad m => EncodeM m (Maybe Bool) (FFI.NothingAsMinusOne Bool) where
-  encodeM = return . FFI.NothingAsMinusOne . maybe (-1) (fromIntegral . fromEnum)
-
-instance Monad m => EncodeM m (Maybe Word) (FFI.NothingAsMinusOne Word) where
-  encodeM = return . FFI.NothingAsMinusOne . maybe (-1) fromIntegral
+-- | an empty 'PassSetSpec'
+defaultPassSetSpec = PassSetSpec {
+  transforms = [],
+  dataLayout = Nothing,
+  targetLibraryInfo = Nothing,
+  targetLowering = Nothing
+}
 
 instance (Monad m, MonadAnyCont IO m) => EncodeM m GCOVVersion CString where
   encodeM (GCOVVersion cs@[_,_,_,_]) = encodeM cs
 
-instance PassManagerSpecification PassSetSpec where
-  createPassManager (PassSetSpec ps tl' dl tli) = flip runAnyContT return $ do
-    let tl = maybe nullPtr (\(TargetMachine tl) -> tl) tl'
-    pm <- liftIO $ FFI.createPassManager
-    forM_ tli $ \(TargetLibraryInfo tli) -> do
-      liftIO $ FFI.addTargetLibraryInfoPass pm tli
-    forM_ dl $ \dl -> liftIO $ withFFIDataLayout dl $ FFI.addDataLayoutPass pm 
-    forM ps $ \p -> $(
-      do
-        TH.TyConI (TH.DataD _ _ _ cons _) <- TH.reify ''Pass
-        TH.caseE [| p |] $ flip map cons $ \con -> do
-          let
-            (n, fns) = case con of
-                          TH.RecC n fs -> (n, [ TH.nameBase fn | (fn, _, _) <- fs ])
-                          TH.NormalC n [] -> (n, [])
-            actions = 
-              [ TH.bindS (TH.varP . TH.mkName $ fn) [| encodeM $(TH.dyn fn) |] | fn <- fns ]
-              ++ [
-               TH.noBindS [|
-                 liftIO $(
-                   foldl1 TH.appE
-                   (map TH.dyn $
-                      ["FFI.add" ++ TH.nameBase n ++ "Pass", "pm"]
-                      ++ ["tl" | FFI.needsTargetLowering (TH.nameBase n)]
-                      ++ fns)
-                  )
-                 |]
-               ]
-          TH.match (TH.conP n $ map (TH.varP . TH.mkName) fns) (TH.normalB (TH.doE actions)) []
-     )
-    return pm
-
-instance PassManagerSpecification [Pass] where
-  createPassManager ps = createPassManager (PassSetSpec ps Nothing Nothing Nothing)
-
-instance PassManagerSpecification ([Pass], TargetLowerin) where
-  createPassManager (ps, tl) = createPassManager (PassSetSpec ps (Just tl) Nothing Nothing)
-
-instance PassManagerSpecification ([Pass], DataLayout, TargetLibraryInfo) where
-  createPassManager (ps, dl, tli) = createPassManager (PassSetSpec ps Nothing (Just dl) (Just tli))
+createPassManager :: PassSetSpec -> IO (Ptr FFI.PassManager)
+createPassManager pss = flip runAnyContT return $ do
+  pm <- liftIO $ FFI.createPassManager
+  case pss of
+    s@CuratedPassSetSpec {} -> liftIO $ do
+      bracket FFI.passManagerBuilderCreate FFI.passManagerBuilderDispose $ \b -> do
+        let handleOption g m = forM_ (m s) (g b <=< encodeM) 
+        handleOption FFI.passManagerBuilderSetOptLevel optLevel
+        handleOption FFI.passManagerBuilderSetSizeLevel sizeLevel
+        handleOption FFI.passManagerBuilderSetDisableUnitAtATime (liftM not . unitAtATime)
+        handleOption FFI.passManagerBuilderSetDisableSimplifyLibCalls (liftM not . simplifyLibCalls)
+        handleOption FFI.passManagerBuilderUseInlinerWithThreshold useInlinerWithThreshold
+        FFI.passManagerBuilderPopulateModulePassManager b pm
+    PassSetSpec ps dl tli tl' -> do
+      let tl = maybe nullPtr (\(TargetMachine tl) -> tl) tl'
+      forM_ tli $ \(TargetLibraryInfo tli) -> do
+        liftIO $ FFI.addTargetLibraryInfoPass pm tli
+      forM_ dl $ \dl -> liftIO $ withFFIDataLayout dl $ FFI.addDataLayoutPass pm 
+      forM_ ps $ \p -> $(
+        do
+          TH.TyConI (TH.DataD _ _ _ cons _) <- TH.reify ''Pass
+          TH.caseE [| p |] $ flip map cons $ \con -> do
+            let
+              (n, fns) = case con of
+                            TH.RecC n fs -> (n, [ TH.nameBase fn | (fn, _, _) <- fs ])
+                            TH.NormalC n [] -> (n, [])
+              actions = 
+                [ TH.bindS (TH.varP . TH.mkName $ fn) [| encodeM $(TH.dyn fn) |] | fn <- fns ]
+                ++ [
+                 TH.noBindS [|
+                   liftIO $(
+                     foldl1 TH.appE
+                     (map TH.dyn $
+                        ["FFI.add" ++ TH.nameBase n ++ "Pass", "pm"]
+                        ++ ["tl" | FFI.needsTargetMachine (TH.nameBase n)]
+                        ++ fns)
+                    )
+                   |]
+                 ]
+            TH.match (TH.conP n $ map (TH.varP . TH.mkName) fns) (TH.normalB (TH.doE actions)) []
+       )
+  return pm
 
 -- | bracket the creation of a 'PassManager'
-withPassManager :: PassManagerSpecification s => s -> (PassManager -> IO a) -> IO a
+withPassManager :: PassSetSpec -> (PassManager -> IO a) -> IO a
 withPassManager s = bracket (createPassManager s) FFI.disposePassManager . (. PassManager)
 
 -- | run the passes in a 'PassManager' on a 'Module', modifying the 'Module'.