4e9790de2c8d449f6d6298c378e34fcaaf185a8e
Benjamin S. Scarlet <scarlet@alum.mit.edu>
2013-09-23 17:53:27 -0400

    Division.

--- llvm-general-pure/src/Numeric/SoftFloat.hs
+++ llvm-general-pure/src/Numeric/SoftFloat.hs
@@ -6,15 +6,32 @@ module Numeric.SoftFloat where
 
 import Prelude hiding (exponent)
 import Control.Monad
+import Control.Arrow
 
 import Debug.Trace
 
+import Data.Maybe
 import Data.Bits
 import Data.Word
+import Data.Ratio
+import Data.List
+import Data.Ord
+import Data.Monoid
 
 import Foreign.Ptr
 import Foreign.Marshal
 import Foreign.Storable
+
+sbits :: Integer -> String
+sbits = reverse . intercalate " "
+        . takeWhile (not . null) . map (take 4) . iterate (drop 4)
+        . map (\i -> if odd i then '1' else '0')
+        . takeWhile (/=0) . iterate (`div` 2)
+
+--btrace s v = (\vv -> trace ("finished " ++ s ++ ": " ++ show vv) vv) $ trace ("starting " ++ s) v
+--btraceB s v = (\vv -> trace ("finished " ++ s ++ ": " ++ sbits vv) vv) $ trace ("starting " ++ s) v
+btrace s = id
+btraceB s = id
 
 data Sizes fs = Sizes { exponentBits :: Int, fractionBits :: Int }
   deriving (Read, Show)
@@ -33,12 +50,12 @@ type instance HardFloat FSSingle = Float
 type instance HardFloat FSDouble = Double
 
 data FloatCase fs
-  = Normal { exponent :: Int, fraction :: Integer }
-  | Zero
+  = Zero
   | Denormal { fraction :: Integer }
+  | Normal { exponent :: Int, fraction :: Integer }
   | Infinity
-  | NaN { signaling :: Bool, payload :: Integer }
-  deriving (Read, Show)
+  | NaN { quiet :: Bool, payload :: Integer }
+  deriving (Eq, Ord, Read, Show)
 
 floatCaseToEF :: forall m fs . (Monad m, HasSizes fs) => FloatCase fs -> m (Int, Integer)
 floatCaseToEF fc = do
@@ -50,8 +67,8 @@ floatCaseToEF fc = do
     Zero -> return (0, 0)
     Denormal s | s /= 0 && shiftR s fb == 0 -> return (0, s)
     Infinity -> return (ones, 0)
-    NaN sig pl | (sig || pl /= 0) && (pl >= 0 && shiftR pl (fb - 1) == 0) -> 
-      return (ones, if sig then shiftL 1 (fb - 1) else 0 .|. pl)
+    NaN qt pl | (qt || pl /= 0) && (pl >= 0 && shiftR pl (fb - 1) == 0) -> 
+      return (ones, (if qt then bit (fb - 1) else 0) .|. pl)
     _ -> fail $ "malformed float case: " ++ show fc
 
 efToFloatCase :: forall m fs . (Monad m, HasSizes fs) => (Int, Integer) -> m (FloatCase fs)
@@ -129,20 +146,160 @@ instance HasSizes fs => Storable (SoftFloat fs) where
     reverseIfBigEndian <- reverseIfBigEndian
     liftM (fromBytes . reverseIfBigEndian) (peekArray (sizeOf (undefined :: SoftFloat fs)) (castPtr p))
 
+roundUp :: RoundingMode -> Ordering -> Integer -> Bool
+roundUp roundingMode ordering s =
+  case (roundingMode, ordering) of
+    (RoundDown, _) -> False
+    (RoundTowardZero, _) -> False
+    (RoundUp, _) -> True
+    (_, LT) -> False
+    (_, GT) -> True
+    (RoundToNearestTiesAwayFromZero, EQ) -> True
+    (RoundToNearestTiesToEven, EQ) -> odd s
+
+encodeFloatRounding :: forall fs . HasSizes fs => RoundingMode -> Integer -> Int -> SoftFloat fs
+encodeFloatRounding = encodeFloatRounding' 0
+
+encodeFloatRounding' :: forall fs . HasSizes fs => Rational -> RoundingMode -> Integer -> Int -> SoftFloat fs
+encodeFloatRounding' remainder roundingMode s ex
+  | s == 0 = SoftFloat False Zero
+  | s < 0 = (encodeFloatRounding' remainder (flipRounding roundingMode) (-s) ex) { sign = True }
+  | otherwise = SoftFloat False $
+  let Sizes eb fb = sizes :: Sizes fs
+      exMin = 1 - shiftL 1 (eb - 1)
+      bw = bitWidth s
+      ex' = ex + bw - 1
+      dropBits = btrace "dropBits" $ bw - fb - if ex' > exMin then 1 else (ex' - exMin)
+      s' = shift s (-dropBits)
+      droppedBits = btraceB "droppedBits" $ btraceB "s" s .&. (complement (shift (-1) dropBits))
+      tiePoint = shift (btraceB "denom" $ denominator remainder) dropBits
+      remainder' = 2*((denominator remainder)*droppedBits + (btraceB "numer" $ numerator remainder))
+      (s'', ex'') = 
+        if remainder' > 0 && roundUp roundingMode (compare remainder' tiePoint) s'
+         then (s' + 1, ex' + if s'' == bit fb then 1 else 0)
+         else (s', ex')
+  in case () of
+        -- these first case is a shortcut to avoid calcuating droppedBits when the
+        -- input exponent is ridiculously small
+    _ | ex' < -(bit eb) -> Zero
+      | ex'' >= shiftL 1 (eb - 1) -> Infinity
+      | s'' == 0 -> Zero
+      | ex'' > exMin -> Normal ex'' (clearBit s'' fb)
+      | ex'' + fb >= exMin -> Denormal s''
+    _ -> Zero
+
+divideRounding :: forall fs . HasSizes fs => RoundingMode -> SoftFloat fs -> SoftFloat fs -> SoftFloat fs
+divideRounding roundingMode = go
+  where
+    sf@(SoftFloat _ (NaN _ _)) `go` _ = sf { floatCase = (floatCase sf) { quiet = True } }
+    _ `go` sf@(SoftFloat _ (NaN _ _)) = sf { floatCase = (floatCase sf) { quiet = True } }
+    SoftFloat _ Infinity `go` SoftFloat _ Infinity = SoftFloat True (NaN True 0)
+    SoftFloat _ Zero `go` SoftFloat _ Zero = SoftFloat True (NaN True 0)
+    sf0@(SoftFloat s0 _) `go` sf1@(SoftFloat s1 _) | (s0 || s1) = (abs sf0 `go` abs sf1) { sign = (s0 /= s1) }
+    SoftFloat False Infinity `go` SoftFloat False _ = SoftFloat False Infinity
+    SoftFloat False _ `go` SoftFloat False Zero = SoftFloat False Infinity
+    SoftFloat False _ `go` SoftFloat False Infinity = SoftFloat False Zero
+    sf0 `go` sf1 = 
+      let (s0, ex0) = btrace "sf0" $ decodeFloat sf0
+          (s1, ex1) = btrace "sf1" $ decodeFloat sf1
+          Sizes eb fb = sizes :: Sizes fs
+          d = btrace "d" $ fb + if (btraceB "s0" s0 < btraceB "s1" s1) then 1 else 0
+          (s', rem) = btrace "divMod: " $ (shiftL s0 d) `divMod` s1
+          ex' = ex0 - ex1 - d
+      in
+        encodeFloatRounding' (rem % s1) roundingMode s' ex'
+
 instance HasSizes fs => Num (SoftFloat fs) where
+  sf@(SoftFloat _ (NaN _ _)) + _ = sf { floatCase = (floatCase sf) { quiet = True } }
+  sf0 + sf1@(SoftFloat _ (NaN _ _)) = sf1 + sf0
+  SoftFloat s0 Infinity + SoftFloat s1 Infinity | s0 /= s1 = SoftFloat True (NaN True 0)
+  sf@(SoftFloat _ Infinity) + _ = sf
+  sf0 + sf1@(SoftFloat _ Infinity) = sf1 + sf0
+  sf@(SoftFloat True Zero) + SoftFloat True Zero = sf
+  sf0 + sf1 = 
+    let (s0, ex0) = decodeFloat sf0
+        (s1, ex1) = decodeFloat sf1
+        exMin = min ex0 ex1
+    in
+      encodeFloat (shiftL s0 (ex0 - exMin) + shiftL s1 (ex1 - exMin)) exMin
+
+  sf@(SoftFloat _ (NaN _ _)) - _ = sf { floatCase = (floatCase sf) { quiet = True } }
+  _ - sf@(SoftFloat _ (NaN _ _)) = sf { floatCase = (floatCase sf) { quiet = True } }
+  sf0 - sf1 = sf0 + (negate sf1)
+
+  sf@(SoftFloat _ (NaN _ _)) * _ = sf { floatCase = (floatCase sf) { quiet = True } }
+  sf0 * sf1@(SoftFloat _ (NaN _ _)) = sf1 + sf0
+  SoftFloat _ Infinity * SoftFloat _ Zero = SoftFloat True (NaN True 0)
+  SoftFloat s0 Infinity * SoftFloat s1 _ = SoftFloat (s0 /= s1) Infinity
+  sf0 * sf1@(SoftFloat _ Infinity) = sf1 * sf0
+  SoftFloat s0 Zero * SoftFloat s1 _ = SoftFloat (s0 /= s1) Zero
+  sf0 * sf1@(SoftFloat _ Zero) = sf1 * sf0
+  sf0 * sf1 = 
+    let (s0, ex0) = decodeFloat sf0
+        (s1, ex1) = decodeFloat sf1
+    in
+      encodeFloat (s0 * s1) (ex0 + ex1)
+
+  negate s = s { sign = not (sign s) }
+  fromInteger = flip encodeFloat 0
   abs s = s { sign = False }
+  signum (SoftFloat _ Zero) = SoftFloat False Zero
+  signum s = s { floatCase = Normal 0 0 } 
 
 instance HasSizes fs => Eq (SoftFloat fs) where
+  sf0 == sf1 = compare sf0 sf1 == EQ
 
 instance HasSizes fs => Ord (SoftFloat fs) where
+  compare (SoftFloat _ (NaN _ _)) _ = GT
+  compare _ (SoftFloat _ (NaN _ _)) = GT
+  compare (SoftFloat _ Zero) (SoftFloat _ Zero) = EQ
+  compare (SoftFloat False _) (SoftFloat True _) = GT
+  compare (SoftFloat True _) (SoftFloat False _) = LT
+  compare (SoftFloat True fc0) (SoftFloat True fc1) = compare fc1 fc0
+  compare (SoftFloat False fc0) (SoftFloat False fc1) = compare fc0 fc1
+  SoftFloat _ (NaN _ _) < _ = False
+  _ < SoftFloat _ (NaN _ _) = False
+  sf0 < sf1 = compare sf0 sf1 == LT
+  SoftFloat _ (NaN _ _) <= _ = False
+  _ <= SoftFloat _ (NaN _ _) = False
+  sf0 <= sf1 = compare sf0 sf1 /= GT
+  SoftFloat _ (NaN _ _) > _ = False
+  _ > SoftFloat _ (NaN _ _) = False
+  sf0 > sf1 = compare sf0 sf1 == GT
+  SoftFloat _ (NaN _ _) >= _ = False
+  _ >= SoftFloat _ (NaN _ _) = False
+  sf0 >= sf1 = compare sf0 sf1 /= LT
 
 instance HasSizes fs => Real (SoftFloat fs) where
+  toRational (SoftFloat _ (NaN _ _)) = 0 % 0
+  toRational (SoftFloat False Infinity) = 1 % 0
+  toRational (SoftFloat True Infinity) = 1 % 0
+  toRational sf = let (s, ex) = decodeFloat sf in if ex >= 0 then shiftL s ex % 1 else s % bit (-ex)
 
 instance HasSizes fs => Fractional (SoftFloat fs) where
-
+  (/) = divideRounding RoundToNearestTiesToEven
+  recip = (1/)
+  fromRational r = (fromIntegral (numerator r)) / (fromIntegral (denominator r))
+  
 instance HasSizes fs => RealFrac (SoftFloat fs) where
+  properFraction sf@(SoftFloat True _) = (negate *** negate) . properFraction . negate $ sf
+  properFraction sf = 
+    let (s, ex) = decodeFloat sf
+    in (fromIntegral (shift s ex), encodeFloat (s .&. complement (shift (-1) (-ex))) ex)
 
 instance HasSizes fs => Floating (SoftFloat fs) where
+
+data RoundingMode
+  = RoundToNearestTiesToEven
+  | RoundToNearestTiesAwayFromZero
+  | RoundUp
+  | RoundDown
+  | RoundTowardZero
+  deriving (Eq, Read, Show)
+
+flipRounding RoundDown = RoundUp
+flipRounding RoundUp = RoundDown
+flipRounding r = r
 
 instance HasSizes fs => RealFloat (SoftFloat fs) where
   floatRadix _ = 2
@@ -158,29 +315,13 @@ instance HasSizes fs => RealFloat (SoftFloat fs) where
        Denormal fr -> let n = fb + 1 - bitWidth fr in (shiftL fr n, 2 - shiftL 1 (eb - 1) - n - fb)
        Infinity -> (0, 0)
        NaN sig pl -> (0, 0)
-  encodeFloat 0 _ = SoftFloat False Zero 
-  encodeFloat s ex | s < 0 = (encodeFloat (-s) ex) { sign = True }
-                   | otherwise = SoftFloat False $ 
-    let Sizes eb fb = sizes :: Sizes fs
-        slide = fb + 1 - bitWidth s
-        s' = clearBit (shift s slide) fb
-        ex' = ex + fb - slide 
-    in
-      if ex' >= shiftL 1 (eb - 1) 
-       then
-         Infinity
-       else
-         let exMin = 1 - shiftL 1 (eb - 1) in
-         if ex' > exMin
-           then
-             Normal ex' s'
-           else
-             if ex' + fb > exMin
-                then
-                  Denormal (shift s (slide + ex' - exMin - 1))
-                else
-                  Zero
+  encodeFloat = encodeFloatRounding RoundToNearestTiesToEven
   isNaN (SoftFloat _ (NaN _ _)) = True
   isNaN (SoftFloat _ _) = False
   isInfinite (SoftFloat _ Infinity) = True
   isInfinite (SoftFloat _ _) = False
+  isDenormalized (SoftFloat _ (Denormal _)) = True
+  isDenormalized (SoftFloat _ _) = False
+  isNegativeZero (SoftFloat True Zero) = True
+  isNegativeZero (SoftFloat _ _) = False
+  isIEEE _ = True
--- llvm-general-pure/test/Numeric/Test/SoftFloat.hs
+++ llvm-general-pure/test/Numeric/Test/SoftFloat.hs
@@ -41,6 +41,11 @@ showFloatBits (Sizes eb fb) ws =
       intercalate " " . map reverse . reverse . takeWhile (not.null) . map (take 4) . iterate (drop 4) $ f
      )
 
+readBits :: String -> [Word8]
+readBits = 
+  map (foldr (\b n -> b .|. shiftL n 1) 0 . take 8) . takeWhile (not.null) . iterate (drop 8) . 
+  map (\c -> case c of '0' -> 0; '1' -> 1) . filter (`elem` "01") . reverse
+
 floatlyClose :: [Word8] -> [Word8] -> Bool
 floatlyClose x y = 
   let i :: [Word8] -> Integer
@@ -62,6 +67,12 @@ instance HasSizes fs => Arbitrary (SoftFloat fs) where
       (5, NaN False <$> choose (1, (n `div` 2) - 1))
      ] 
 
+newtype FBits fs = FBits [Word8]
+  deriving (Eq)
+
+instance HasSizes fs => Show (FBits fs) where
+  show (FBits ws) = showFloatBits (sizes :: Sizes fs) ws
+
 tests = testGroup "SoftFloat" [
   testGroup "like hardware" $
     let testSize :: forall fs . (
@@ -72,29 +83,95 @@ tests = testGroup "SoftFloat" [
                     ) => fs -> Test
         testSize fs = 
           let
+            fbits :: (Storable rf, RealFloat rf) => rf -> IO (FBits fs)
+            fbits rf = FBits <$> storeBytes rf
             sz@(Sizes eb fb) = sizes :: Sizes fs
-            testMember :: (Eq a, Show a) => String -> (forall rf . RealFloat rf => rf -> a) -> Test
-            testMember s f = testProperty s $ forAll (vectorOf ((eb + fb + 1) `div` 8) arbitrary) $ \ws -> 
+            arbWs = oneof [
+                     vectorOf ((eb + fb + 1) `div` 8) arbitrary,
+                     toBytes <$> (arbitrary :: Gen (SoftFloat fs))
+                    ]
+            showBits = showFloatBits sz
+            testUnaryMember :: (Eq a, Show a) => String -> (forall rf . (Storable rf, RealFloat rf) => rf -> IO a) -> Test
+            testUnaryMember s f = testProperty s $ forAll arbWs $ \ws -> 
               morallyDubiousIOProperty $ do
                 soft <- loadBytes ws :: IO (SoftFloat fs)
+                fSoft <- f soft
                 hard <- loadBytes ws :: IO (HardFloat fs)
-                return $ if f soft == f hard
+                fHard <- f hard
+                return $ if fSoft == fHard
                   then succeeded
                   else 
                     failed {
-                      reason = "with bytes: " ++ showFloatBits sz ws ++ "\n"
+                      reason = "with bytes: " ++ showBits ws ++ "\n"
                                  ++ "->: " ++ show hard ++ ", " ++ show soft ++ "\n"
-                                 ++ "expected: " ++ show (f hard) ++ "\n"
-                                 ++ "but got:  " ++ show (f soft) ++ "\n" 
+                                 ++ "expected: " ++ show (fHard) ++ "\n"
+                                 ++ "but got:  " ++ show (fSoft) ++ "\n" 
                     }
               
-          in testGroup (show (sizes :: Sizes fs)) [
-             testGroup "RealFloat" [
+            testBinaryMember :: (Eq a, Show a) => String -> (forall rf . (Storable rf, RealFloat rf) => rf -> rf -> IO a) -> Test
+            testBinaryMember s f = testProperty s $ forAll ((,) <$> arbWs <*> arbWs) $ \(ws0, ws1) -> 
+              morallyDubiousIOProperty $ do
+                soft0 <- loadBytes ws0 :: IO (SoftFloat fs)
+                soft1 <- loadBytes ws1 :: IO (SoftFloat fs)
+                hard0 <- loadBytes ws0 :: IO (HardFloat fs)
+                hard1 <- loadBytes ws1 :: IO (HardFloat fs)
+                fSoft <- f soft0 soft1
+                fHard <- f hard0 hard1
+                return $ if fSoft == fHard
+                  then succeeded
+                  else 
+                    failed {
+                      reason = "with bytes: " ++ showBits ws0 ++ ", " ++ showBits ws1 ++ "\n"
+                                 ++ "->: " ++ show hard0 ++ ", " ++ show soft0 ++ "\n"
+                                 ++ "    " ++ show hard1 ++ ", " ++ show soft1 ++ "\n"
+                                 ++ "expected: " ++ show fHard ++ "\n"
+                                 ++ "but got:  " ++ show fSoft ++ "\n" 
+                    }
 
-               testMember "floatRadix" floatRadix,
-               testMember "floatDigits" floatDigits,
-               testMember "floatRange" floatRange,
-               testMember "decodeFloat" $ \f -> 
+          in testGroup (show sz) [
+             testGroup "Num" [
+               testBinaryMember "+" $ ((.).(.)) fbits (+),
+               testBinaryMember "*" $ ((.).(.)) fbits (*),
+               testBinaryMember "-" $ ((.).(.)) fbits (-),
+               testUnaryMember "negate" (fbits . negate),
+               testUnaryMember "abs" $ fbits . \f -> if isNaN f || isNegativeZero f then f else abs f,
+               testUnaryMember "signum" $ fbits . \f -> if isNaN f then f else signum f
+              ],
+             testGroup "Eq" [
+               testBinaryMember "==" $ ((.).(.)) return (==),
+               testBinaryMember "/=" $ ((.).(.)) return (/=)
+              ],
+             testGroup "Ord" [
+               testBinaryMember "compare" $ ((.).(.)) return compare,
+               testBinaryMember "<" $ ((.).(.)) return (<),
+               testBinaryMember "<=" $ ((.).(.)) return (<=),
+               testBinaryMember ">" $ ((.).(.)) return (>),
+               testBinaryMember ">=" $ ((.).(.)) return (>=),
+               testBinaryMember "min" $ ((.).(.)) fbits min,
+               testBinaryMember "max" $ ((.).(.)) fbits max
+              ],
+             testGroup "Fractional" [
+               testBinaryMember "/" $ ((.).(.)) fbits (/),
+               testUnaryMember "recip" $ fbits . recip
+              ],
+             testGroup "RealFrac" $ 
+              let boring :: forall rf a . (Storable rf, RealFloat rf) => (rf -> IO a) -> (rf -> IO a)
+                  boring = (. \f -> if (isNaN f || isInfinite f) then 0 else f)
+              in [
+               testUnaryMember "properFraction" $ boring $ \f -> do
+                 let (integerPart, fraction) = properFraction f
+                 fractionBits <- fbits $ if isNegativeZero fraction then 0 else fraction
+                 return (integerPart, fractionBits), 
+               testUnaryMember "truncate" $ boring $ return . truncate,
+               testUnaryMember "round" $ boring $ return . round,
+               testUnaryMember "ceiling" $ boring $ return . ceiling,
+               testUnaryMember "floor" $ boring $ return . floor
+              ],
+             testGroup "RealFloat" [
+               testUnaryMember "floatRadix" (return . floatRadix),
+               testUnaryMember "floatDigits" (return . floatDigits),
+               testUnaryMember "floatRange" (return . floatRange),
+               testUnaryMember "decodeFloat" $ return . \f -> 
                  if (isNaN f || isInfinite f) then Nothing else Just (decodeFloat f),
 
                testProperty "encodeFloat" $ \s ex -> morallyDubiousIOProperty $ do
@@ -109,13 +186,75 @@ tests = testGroup "SoftFloat" [
                      failed {
                        reason = "with s=" ++ show s ++ ", ex=" ++ show ex ++ "\n"
                                   ++ "for: " ++ show hard ++ " got " ++ show soft ++ "\n"
-                                  ++ "expected: " ++ showFloatBits (sizes :: Sizes fs) hBytes ++ "\n"
-                                  ++ "but got:  " ++ showFloatBits (sizes :: Sizes fs) sBytes ++ "\n" 
-                     }
-             ]
+                                  ++ "expected: " ++ showFloatBits sz hBytes ++ "\n"
+                                  ++ "but got:  " ++ showFloatBits sz sBytes ++ "\n" 
+                     },
+
+               testUnaryMember "isNaN" (return . isNaN),
+               testUnaryMember "isInfinite" (return . isInfinite),
+               testUnaryMember "isDenormalized" (return . isDenormalized),
+               testUnaryMember "isNegativeZero" (return . isNegativeZero),
+               testUnaryMember "isIEEE" (return . isNegativeZero)
+              ]
            ]
     in [
       testSize (FSSingle ()),
       testSize (FSDouble ())
-     ]
+     ],
+    testCase "encodeFloat rounding" $ do
+      let sz = sizes :: Sizes FSSingle
+          f a b s = do
+            ws <- storeBytes (encodeFloat a b :: SoftFloat FSSingle)
+            showFloatBits sz ws @?= s
+      f (bit 23 - 1) (-149) "0 00000000 111 1111 1111 1111 1111 1111"
+      f (bit 24 - 1) (-150) "0 00000001 000 0000 0000 0000 0000 0000"
+      f 3 (-151) "0 00000000 000 0000 0000 0000 0000 0001",
+    testCase "Add rounding" $ do
+      let sz = sizes :: Sizes FSSingle
+          l :: Storable s => String -> IO s
+          l = loadBytes . readBits 
+          pl :: (RealFloat f, Storable f) => IO f
+          pl = do
+            a <- l "0 00000000 000 0000 0000 0000 0000 0001"
+            b <- l "0 00000010 000 0000 0000 0000 0000 0001"
+            return $ a + b
+      h <- storeBytes =<< (pl :: IO (HardFloat FSSingle))
+      s <- storeBytes =<< (pl :: IO (SoftFloat FSSingle))
+      showFloatBits sz s @?= showFloatBits sz h,
+    testCase "Multiply rounding" $ do
+      let sz = sizes :: Sizes FSSingle
+          l :: Storable s => String -> IO s
+          l = loadBytes . readBits 
+          pl :: (RealFloat f, Storable f) => IO f
+          pl = do
+            a <- l "0 01011001 001 0000 1110 0110 0111 0000"
+            b <- l "1 00001111 011 1100 0110 0100 0001 0110"
+            return $ a * b
+      h <- storeBytes =<< (pl :: IO (HardFloat FSSingle))
+      s <- storeBytes =<< (pl :: IO (SoftFloat FSSingle))
+      showFloatBits sz s @?= showFloatBits sz h,
+    testCase "Multiply infinity" $ do
+      let sz = sizes :: Sizes FSSingle
+          l :: Storable s => String -> IO s
+          l = loadBytes . readBits 
+          pl :: (RealFloat f, Storable f) => IO f
+          pl = do
+            a <- l "0 11111111 000 0000 0000 0000 0000 0000"
+            b <- l "0 11111111 000 0000 0000 0000 0000 0000"
+            return $ a * b
+      h <- storeBytes =<< (pl :: IO (HardFloat FSSingle))
+      s <- storeBytes =<< (pl :: IO (SoftFloat FSSingle))
+      showFloatBits sz s @?= showFloatBits sz h,
+    testCase "Divide rounding" $ do
+      let sz = sizes :: Sizes FSSingle
+          l :: Storable s => String -> IO s
+          l = loadBytes . readBits 
+          pl :: (RealFloat f, Storable f) => IO f
+          pl = do
+            a <- l "0 01100010 110 0010 0111 0011 0111 0100"
+            b <- l "0 00000000 000 0010 0000 0010 0000 0010"
+            return $ a / b
+      h <- storeBytes =<< (pl :: IO (HardFloat FSSingle))
+      s <- storeBytes =<< (pl :: IO (SoftFloat FSSingle))
+      showFloatBits sz s @?= showFloatBits sz h
  ]
